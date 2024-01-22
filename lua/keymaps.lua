1e8eedb4eeee202a70195441b71f98343f62a878
Felipe Godoi <feeh.08.2001@gmail.com>
2024-01-21 23:39:29 -0300

    reorganize keymaps to separate in one file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,17 @@
+-- remap leader binding
 vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 
+-- toggle trouble window 
 vim.keymap.set("n", "<leader>t", function() require("trouble").toggle() end)
+
+-- fzf-lua bindings
+vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-f>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-l>", [[<Cmd>lua require"fzf-lua".live_grep_glob()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
+
+-- toggle NvimTreeTroggle
+vim.keymap.set('n', 'T', "<cmd>:NvimTreeToggle<CR>", { silent = true })
--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,3 +1,2 @@
 require("nvim-tree").setup()
 
-vim.keymap.set('n', 'T', "<cmd>:NvimTreeToggle<CR>", { silent = true })
